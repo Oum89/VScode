de317f3751b5abc0a44608f410bca30cd641f4cf
Oum89 <oumayma.naaim.ext@holcim.com>
2023-07-19 14:32:58 +0200

    commit

--- pyDev/code1.py
+++ pyDev/code1.py
@@ -1,1 +1,26 @@
-print("display this on branch Feature2")
+
+import pandas as pd
+import os
+from tabulate import tabulate
+
+path = format(os.environ['RootPath'])
+file_path = path + '/pyProject/VScode/pyOutput/' + 'data.csv'
+# Sample data for the DataFrame
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [25, 30, 22, 28],
+    'City': ['New York', 'London', 'Paris', 'Tokyo']
+}
+
+# Create the DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print("DataFrame is:")
+print(tabulate(df, headers='keys', tablefmt='psql'))
+
+# Save the DataFrame to a CSV file
+
+df.to_csv(file_path, sep='\t', index=False)
+
+print(f"DataFrame saved to {file_path}")
