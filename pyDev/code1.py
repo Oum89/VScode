d2832be927df197a44065ab8b56d45a8bef0192a
Oum89 <oumayma.naaim.ext@holcim.com>
2023-07-20 09:44:56 +0200

    feat1

--- pyDev/code1.py
+++ pyDev/code1.py
@@ -1,26 +1,2 @@
-
-import pandas as pd
-import os
-from tabulate import tabulate
-
-path = format(os.environ['RootPath'])
-file_path = path + '/pyProject/VScode/pyOutput/' + 'data.csv'
-# Sample data for the DataFrame
-data = {
-    'Name': ['valentin', 'Bob', 'Charlie', 'David'],
-    'Age': [25, 30, 22, 28],
-    'City': ['New York', 'Chicago', 'Paris', 'Rabat']
-}
-
-# Create the DataFrame
-df = pd.DataFrame(data)
-
-# Print the DataFrame
-print("DataFrame:")
-print(tabulate(df, headers='keys', tablefmt='psql'))
-
-# Save the DataFrame to a CSV file
-
-df.to_csv(file_path, sep='\t', index=False)
-
-print(f"DataFrame saved to {file_path}")
+for i in range (6):
+    print(i)
